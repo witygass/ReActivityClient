7b7d5812ab5815c595d15a38858a5800ecd2ebeb
Michael Ilg <michael@ilg.ac>
2017-01-04 11:59:56 -0800

    Integrate socketIO with ngrok

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -8,11 +8,19 @@ import {
   ExponentLinksView,
 } from '@exponent/samples';
 
+import socket from '../components/SocketIo';
+
 export default class ProfileScreen extends React.Component {
   static route = {
     navigationBar: {
       title: 'Profile',
     },
+  }
+  componentDidMount() {
+    socket.on('news', function (data) {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
   }
 
   render() {
@@ -20,7 +28,7 @@ export default class ProfileScreen extends React.Component {
       <ScrollView
         style={styles.container}
         contentContainerStyle={this.props.route.getContentContainerStyle()}>
-        <Text>This is where the profile screen will live</Text>
+        <Text>This is where the profile screen will live and be not very happy</Text>
       </ScrollView>
     );
   }
