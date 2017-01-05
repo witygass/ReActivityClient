09ac12bd1756342d190ae21ca8ecc49cf16bb4cb
witygass <witygass@indiana.edu>
2017-01-05 11:13:42 -0800

    fixed merge issues

--- components/SocketIo.js
+++ components/SocketIo.js
@@ -1,9 +1,6 @@
 import io from 'socket.io-client';
-<<<<<<< 8252161a86053ffd7b3dc1662fc848c2d94cf23e
 import { ngrokURL } from '../lib/localvars.js';
+
 const socket = io.connect(ngrokURL);
-=======
-const socket = io.connect(' http://652f4185.ngrok.io');
->>>>>>> Integrate socketIO with ngrok
 
 module.exports = socket;
