a0872224a9b71bcaf12a9f6daf84301dfc78a522
Jarob Gilliam <jarobgilliambiz@gmail.com>
2017-01-10 12:27:49 -0800

    fix event view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -27,7 +27,6 @@ import { MonoText } from '../components/StyledText';
 import dummyEventData from './dummyData/dummyEventData';
 import {api} from '../lib/ajaxCalls'
 import {store} from '../lib/reduxStore'
-import {generator} from '../lib/dataGeneration';
 
 export default class HomeScreen extends React.Component {
   static route = {
@@ -56,16 +55,12 @@ export default class HomeScreen extends React.Component {
 
   _onRefresh() {
     var that = this;
-    // this.setState({refreshing: true});
     api.getNearbyEvents({}, function(events) {
       store.dispatch({
         type: 'UPDATE_NEARBY_EVENT_TABLE',
         events: events
       });
       that.setState({nearbyEvents: events});
-      console.log('state is:', that.state.nearbyEvents);
-      // that.setState({refreshing: false});
-
     })
 
     AsyncStorage.getItem('JWTtoken').then((token) => {
@@ -77,7 +72,6 @@ export default class HomeScreen extends React.Component {
 
   hotRefresh() {
     this.setState({currentlyViewing: store.getState().currentlyViewing});
-    console.log('currentlyViewing has changed. It is now:', store.getState().currentlyViewing);
   }
 
   componentWillMount() {
@@ -89,7 +83,6 @@ export default class HomeScreen extends React.Component {
       events: events
     });
     that.setState({nearbyEvents: events});
-    console.log('state is:', that.state.nearbyEvents);
   });
 
       AsyncStorage.getItem('JWTtoken').then((token) => {
@@ -136,94 +129,6 @@ export default class HomeScreen extends React.Component {
   }
 }
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  // render() {
-  //   return (
-  //     <View style={styles.container}>
-  //       <ScrollView
-  //         style={styles.container}
-  //         contentContainerStyle={styles.contentContainer}>
-  //
-  //         <View style={styles.welcomeContainer}>
-  //           <Image
-  //             source={require('../assets/images/exponent-wordmark.png')}
-  //             style={styles.welcomeImage}
-  //           />
-  //         </View>
-  //
-  //
-  //         <View style={styles.getStartedContainer}>
-  //           {this._maybeRenderDevelopmentModeWarning()}
-  //
-  //           <Text style={styles.getStartedText}>
-  //             Get started by opening
-  //           </Text>
-  //
-  //           <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-  //             <MonoText style={styles.codeHighlightText}>
-  //               screens/HomeScreen.js
-  //             </MonoText>
-  //           </View>
-  //
-  //           <Text style={styles.getStartedText}>
-  //             Change this text and your app will automatically reload.
-  //           </Text>
-  //         </View>
-  //
-  //         <View style={styles.helpContainer}>
-  //           <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-  //             <Text style={styles.helpLinkText}>
-  //               Help, it didn’t automatically reload!
-  //             </Text>
-  //           </TouchableOpacity>
-  //         </View>
-  //       </ScrollView>
-  //
-  //       <View style={styles.tabBarInfoContainer}>
-  //         <Text style={styles.tabBarInfoText}>
-  //           This is a tab bar. You can edit it in:
-  //         </Text>
-  //
-  //         <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-  //           <MonoText style={styles.codeHighlightText}>
-  //             navigation/RootNavigation.js
-  //           </MonoText>
-  //         </View>
-  //       </View>
-  //     </View>
-  //   );
-  // }
-
-  // _maybeRenderDevelopmentModeWarning() {
-  //   if (__DEV__) {
-  //     const learnMoreButton = (
-  //       <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-  //         Learn more
-  //       </Text>
-  //     );
-  //
-  //     return (
-  //       <Text style={styles.developmentModeText}>
-  //         Development mode is enabled, your app will run slightly slower but
-  //         you have access to useful development tools. {learnMoreButton}.
-  //       </Text>
-  //     );
-  //   } else {
-  //     return (
-  //       <Text style={styles.developmentModeText}>
-  //         You are not in development mode, your app will run at full speed.
-  //       </Text>
-  //     );
-  //   }
-  // }
-  //
-  // _handleLearnMorePress = () => {
-  //   Linking.openURL('https://docs.getexponent.com/versions/latest/guides/development-mode');
-  // }
-  //
-  // _handleHelpPress = () => {
-  //   Linking.openURL('https://docs.getexponent.com/versions/latest/guides/up-and-running.html#can-t-see-your-changes');
-  // }
 
 var {height, width} = Dimensions.get('window');
 
