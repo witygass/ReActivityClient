e86ec5642237504ebb52343715e06f2cdbfc1751
witygass <witygass@indiana.edu>
2017-01-18 22:14:26 -0800

    styled mapview custom callout

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -11,6 +11,7 @@ import MapView from 'react-native-maps';
 import EventMarkerCallout from '../components/EventMarkerCallout';
 import { store } from '../lib/reduxStore';
 import { FontAwesome } from '@exponent/vector-icons';
+import { loader } from '../lib/loader';
 var {height, width} = Dimensions.get('window');
 
 export default class MapScreen extends React.Component {
@@ -32,10 +33,12 @@ export default class MapScreen extends React.Component {
       },
       events: store.getState().nearbyEvents
     }
+    // this.onRegionChange = this.onRegionChange.bind(this);
   }
 
   componentDidMount() {
-    var context = this;
+    console.log(this.state.events);
+    var mapView = this;
     async function getLocationAsync() {
       const { Location, Permissions } = Exponent;
       const { status }  = await Permissions.askAsync(Permissions.LOCATION);
@@ -46,40 +49,42 @@ export default class MapScreen extends React.Component {
         navigator.geolocation.getCurrentPosition((position) => {
           console.log('location permission is granted.');
           var initialPosition = position;
-          context.setState({initialPosition});
+          mapView.setState({initialPosition});
         },
         (error) => alert(JSON.stringify(error))
         );
-        context.watchID = navigator.geolocation.watchPosition((position) => {
-          // console.log('watching device location now');
+        mapView.watchID = navigator.geolocation.watchPosition((position) => {
           var currentPosition = position.coords;
-          var locs = [];
-          var latDelta = 0;
-          var lonDelta = 0;
-          var userLat = currentPosition.latitude;
-          var userLon = currentPosition.longitude;
-          for (let i = 0; i < context.state.events.length; i++) {
-            var eventCoords = context.state.events[i].locDetailsView
-            if (Math.abs(userLat - eventCoords.latitude) * 2.2 > latDelta) {
-              latDelta = Math.abs(userLat - eventCoords.latitude) * 2.2
-            }
-            if (Math.abs(userLon - eventCoords.longitude) * 2.2 > lonDelta) {
-              lonDelta = Math.abs(userLon - eventCoords.longitude) * 2.2
-            }
-          }
-          currentPosition.latitudeDelta = latDelta;
-          currentPosition.longitudeDelta = lonDelta;
-          context.setState({region: currentPosition});
+          var latMin, latMax, lonMin, lonMax;
+          mapView.state.events.forEach((event) => {
+            if (latMin === undefined || event.locDetailsView.latitude < latMin) { latMin = event.locDetailsView.latitude }
+            if (lonMin === undefined || event.locDetailsView.longitude < lonMin) { lonMin = event.locDetailsView.longitude }
+            if (latMax === undefined || event.locDetailsView.latitude > latMax) { latMax = event.locDetailsView.latitude }
+            if (lonMax === undefined || event.locDetailsView.longitude > lonMax) { lonMax = event.locDetailsView.longitude }
+          });
+          currentPosition.latitudeDelta = (latMax - latMin); // latDelta;
+          currentPosition.longitudeDelta = (lonMax - lonMin); // lonDelta;
+          console.log(currentPosition.latitudeDelta, currentPosition.longitudeDelta);
+          mapView.setState({region: currentPosition});
         });
       } else {
         throw new Error('Location permission not granted');
       }
-      currentPosition.latitudeDelta = latDelta;
-      currentPosition.longitudeDelta = lonDelta;
-      context.setState({region: currentPosition});
     };
     getLocationAsync();
   }
+
+  // onRegionChange(region) {
+  //   var map = this;
+  //   map.setState({region});
+  //   loader.loadNearbyEvents(function(events) {
+  //     map.setState({events: events});
+  //   }, region.latitude, region.longitude);
+  // }
+  // ADD THIS TO THE MAPVIEW
+  ///////////////////////////////////////
+  // onRegionChange={this.onRegionChange}
+  ///////////////////////////////////////
 
   render() {
     return (
@@ -93,13 +98,15 @@ export default class MapScreen extends React.Component {
           coordinate={{longitude: eventMarker.locDetailsView.longitude, latitude: eventMarker.locDetailsView.latitude}}
           title={eventMarker.title}
           description={eventMarker.description}
-          key={eventMarker.id}
+          key={eventMarker.title}
         >
           <MapView.Callout
             style={{width: width * .75}}
             tooltip={false}
           >
-            <EventMarkerCallout event={eventMarker}/>
+            <EventMarkerCallout
+              event={eventMarker}
+            />
           </MapView.Callout>
         </MapView.Marker>
       ))}
